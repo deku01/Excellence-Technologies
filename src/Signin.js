dff5283893acaf25bf2b08ae637281a8e416c7c3
deku01 <deku37@gmail.com>
2021-02-24 09:50:06 +0530

    updated code

--- src/Signin.js
+++ src/Signin.js
@@ -1,52 +1,52 @@
-import React, {useState} from 'react';
-import { TextInput,View,Alert,Button,StyleSheet,Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
+import React from 'react';
+import { TextInput,View,Button,StyleSheet,Text } from 'react-native';
 
-const Signin = () => {
-  const [userValue, onChangeUser] = useState('Username');
-  const [passValue, onChangePass] = useState('Password');
-
-  return (
+const Signin = () => (
+  <>
+    <StatusBar />
     <View style={styles.container}>
-    <Text style={styles.title}>My Apps</Text>
-    <TextInput style={styles.textInput}
-      onChangeUser={text => onChangeUser(text)}
-      placeholder={userValue}
-      userValue={userValue}
-    />
-    <TextInput style={styles.textInput}
-      onChangePass={text => onChangePass(text)}
-      placeholder={passValue}
-      passValue={passValue}
-    />
-     <Button
-        title="Submit"
-        color="#21908E" 
-        onPress={() => Alert.alert('Submitted')}
-      />
+      <View style={styles.header}>
+        <Text style={styles.text}>My App</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput style={styles.input} placeholder="Username" />
+        <TextInput style={styles.input} placeholder="Password" />
+        <View style={{ marginTop: 20 }}>
+          <Button title="Submit" color="#008891" />
+        </View>
+      </View>
     </View>
-  );
-}
+  </>
+)
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      marginHorizontal: 16,
-    },
-    textInput: {
-        height: 40, 
-        borderColor: 'green', 
-        borderWidth: 1, 
-        marginBottom:15,
-        padding:10,
-        color: 'grey',
-       },
-       title: {
-        fontSize: 15,
-        backgroundColor: "#21908E",
-        padding: 10,
-        color: "white",
-       }
-  })
+  container: {
+    backgroundColor: '#e7e7de',
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  header: {
+    backgroundColor: '#008891',
+    padding: 15,
+  },
+  text: {
+    color: '#e7e7de',
+    fontSize: 18,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    borderColor: '#008891',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+});
 
 export default Signin;
